8e8755f0d9d830420dac3ef00a7d66af70cc8a5b
Brian J. Strout <brianjstrout@gmail.com>
2023-11-23 18:43:57 -0500

    made email form live

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
-document.addEventListener("click", () => {
+document.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn-primary")) {
     e.preventDefault();
     const formSelector = document.getElementById("myForm");
 
-    let formData = {
-      tes: "test",
-      service_id: "service_9moydam",
-      template_id: "template_fejt36a",
-      form: document.getElementById("myForm"),
-      user_id: "sMWYJVw_Of2YBvfKm",
-      template_params: {
-        "sender-name": "James",
-        "sender-email": "test@fake.com",
-      },
-    };
+    // let formData = {
+    //   tes: "test",
+    //   service_id: "service_9moydam",
+    //   template_id: "template_fejt36a",
+    //   form: document.getElementById("myForm"),
+    //   user_id: "sMWYJVw_Of2YBvfKm",
+    //   template_params: {
+    //     "sender-name": "James",
+    //     "sender-email": "test@fake.com",
+    //   },
+    // };
 
-    let b = {
-      bname: formSelector.name,
-    };
-    let bbname = formSelector.name.value;
+    // let b = {
+    //   bname: formSelector.name,
+    // };
+    // let bbname = formSelector.name.value;
 
     console.log("submitting");
-    console.log(b);
+    // console.log(b);
 
     const serviceID = "service_9moydam";
-    const templateID = "template_fejt36a";
-    const tt = formData.tes;
+    const templateID = "template_u3la1xn";
+    // const tt = formData.tes;
     const params = {
       fromName: document.getElementById("fromName").value,
       fromEmail: document.getElementById("fromEmail").value,
-      message: document.getElementById("date").value,
+      date: document.getElementById("date").value,
     };
 
     console.log(params);
 
     console.log("^^^ params");
 
-    // "sMWYJVw_Of2YBvfKm",
-    // send the email here
-    emailjs
-      .send(serviceID, templateID, params)
-      // .sendForm(JSON.stringify(formData))
-      .then((response) => {
-        console.log(params);
+    emailjs.send(serviceID, templateID, params).then((response) => {
+      console.log(params);
 
-        // console.log("Thanks for your email!", response.status, response.text);
-        alert("Thanks for your email!");
-      });
+      alert("All set, thanks!");
+    });
     return;
   }
 });
